cf0d1ec5a217a8f04476d57e57c3ed1d8c0c4d11
Serhii Stepanovych <hunterla2@gmail.com>
2019-10-06 02:36:23 +0300

    Parse robots

--- CodingGame/Program.cs
+++ CodingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 /**
 * Deliver more ore to hq (left side of the map) than your opponent. Use radars to find ore but beware of traps!
@@ -18,6 +19,8 @@ namespace CodingGame
             // game loop
             while (true)
             {
+                GameData.InitializeTurn();
+
                 Input.ParseTurn();
 
                 for (int i = 0; i < 5; i++)
@@ -26,6 +29,119 @@ namespace CodingGame
                 }
             }
         }
+    }
+
+    static class GameData
+    {
+        public static void InitializeTurn()
+        {
+            MyRobots = new List<Robot>();
+            OpponentRobots = new List<Robot>();
+        }
+
+        public static List<Robot> MyRobots { get; set; }
+        public static List<Robot> OpponentRobots { get; set; }
+    }
+
+    class Coordinate
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        #region Constructors and overriden methods
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"X={X}, Y={Y}";
+        }   
+
+        public override bool Equals(object obj)
+        {
+            Coordinate other = obj as Coordinate;
+
+            if (ReferenceEquals(null, obj))
+                return false;
+
+            if (X == other.X && Y == other.Y)
+                return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 7333 ^ X.GetHashCode() ^ Y.GetHashCode();
+        }
+
+        #endregion Constructors and overriden methods
+    }
+
+    class Robot
+    {
+        private EntityType _type;
+
+        public int Id { get; private set; }
+
+        public Coordinate Coordinate { get; private set; }
+
+        public EntityType Item { get; private set; }
+
+        #region Constructors and overriden methods
+
+        public Robot(int id, EntityType type, Coordinate coordinate, EntityType item)
+        {
+            Id = id;
+            _type = type;
+            Coordinate = coordinate;
+            Item = item;
+        }
+
+        public override string ToString()
+        {
+            var owner = IsMine ? "My" : "Op";
+            return $"{owner}Robot. Id={Id}. {Coordinate}. Item={Item}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Robot other = obj as Robot;
+
+            if (ReferenceEquals(null, obj))
+                return false;
+
+            if (Id == other.Id)
+                return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 7333 ^ Id.GetHashCode();
+        }
+
+        #endregion Constructors and overriden methods
+
+        public bool IsMine => _type == EntityType.MyRobot;
+    }
+
+    enum EntityType
+    {
+        None = -1,
+        MyRobot = 0,
+        OpponentRobot = 1,
+        Radar = 2,
+        Trap = 3,
+        Ore = 4
+
+        // 0 for your robot, 1 for other robot, 2 for radar, 3 for trap
+        // if this entity is a robot, the item it is carrying (-1 for NONE, 2 for RADAR, 3 for TRAP, 4 for ORE)
     }
 
     #region Read input data
@@ -57,6 +173,12 @@ namespace CodingGame
                     int hole = int.Parse(inputs[2 * j + 1]);// 1 if cell has a hole
                 }
             }
+
+            ParseEntities();
+        }
+
+        private static void ParseEntities()
+        {
             inputs = Console.ReadLine().Split(' ');
             int entityCount = int.Parse(inputs[0]); // number of entities visible to you
             int radarCooldown = int.Parse(inputs[1]); // turns left until a new radar can be requested
@@ -65,10 +187,25 @@ namespace CodingGame
             {
                 inputs = Console.ReadLine().Split(' ');
                 int id = int.Parse(inputs[0]); // unique id of the entity
-                int type = int.Parse(inputs[1]); // 0 for your robot, 1 for other robot, 2 for radar, 3 for trap
+                EntityType type = (EntityType)int.Parse(inputs[1]); // 0 for your robot, 1 for other robot, 2 for radar, 3 for trap
+
                 int x = int.Parse(inputs[2]);
                 int y = int.Parse(inputs[3]); // position of the entity
-                int item = int.Parse(inputs[4]); // if this entity is a robot, the item it is carrying (-1 for NONE, 2 for RADAR, 3 for TRAP, 4 for ORE)
+                var coordinate = new Coordinate(x, y);
+
+                EntityType item = (EntityType)int.Parse(inputs[4]); // if this entity is a robot, the item it is carrying (-1 for NONE, 2 for RADAR, 3 for TRAP, 4 for ORE)
+
+                if (type == EntityType.MyRobot || type == EntityType.OpponentRobot)
+                {
+                    var robot = new Robot(id, type, coordinate, item);
+
+                    if (robot.IsMine)
+                        GameData.MyRobots.Add(robot);
+                    else
+                        GameData.OpponentRobots.Add(robot);
+
+                    Console.Error.WriteLine($"Parsing robot: {robot}");
+                }
             }
         }
     }
