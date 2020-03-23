067797359270bbce8caf7643f63df990339b3c08
craHUNzyTER <hunterla2@gmail.com>
2020-03-23 10:28:29 +0200

    Download base game code

--- CodingGame/Program.cs
+++ CodingGame/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
+// Write an action using Console.WriteLine()
+// To debug: Console.Error.WriteLine("Debug messages...");
 namespace CodingGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputs;
+            inputs = Console.ReadLine().Split(' ');
+            int width = int.Parse(inputs[0]);
+            int height = int.Parse(inputs[1]);
+            int myId = int.Parse(inputs[2]);
+            for (int i = 0; i < height; i++)
+            {
+                string line = Console.ReadLine();
+            }
+
+            Console.WriteLine("7 7");
+
+            // game loop
+            while (true)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int x = int.Parse(inputs[0]);
+                int y = int.Parse(inputs[1]);
+                int myLife = int.Parse(inputs[2]);
+                int oppLife = int.Parse(inputs[3]);
+                int torpedoCooldown = int.Parse(inputs[4]);
+                int sonarCooldown = int.Parse(inputs[5]);
+                int silenceCooldown = int.Parse(inputs[6]);
+                int mineCooldown = int.Parse(inputs[7]);
+                string sonarResult = Console.ReadLine();
+                string opponentOrders = Console.ReadLine();
+
+                Console.WriteLine("MOVE N TORPEDO");
+            }
         }
     }
 }
